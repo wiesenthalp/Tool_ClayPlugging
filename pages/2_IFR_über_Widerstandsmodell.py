c4b844f1bb457aeead90787676306ebb44cf960f
Philipp Wiesenthal <p.wiesenthal@gmail.com>
2025-04-17 16:57:49 +0200

    Update 2_IFR_über_Widerstandsmodell.py

--- pages/2_IFR_über_Widerstandsmodell.py
+++ pages/2_IFR_über_Widerstandsmodell.py
@@ -27,7 +27,8 @@ def eingabe_bodenschichten():
     df = pd.DataFrame(default_data)
     df_display = np.transpose(df)
     for i in range(len(df_display)):
-        df_display[f"Schicht {i+1}"] = df_display[i]
+        new_key = f"Schicht {i+1}"
+        df_display[new_key] = df_display[i]
         del df_display[i]
     edited_df = st.data_editor(df_display, num_rows="dynamic", key="boden_tabelle")
 
